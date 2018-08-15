f78c5fc4bf4978cf227eb9998b06dba013ad1cb1
Alexey Panteleychuk <alexeypanteleychuk@MacBook-Pro-Alexey.local>
2018-08-15 14:34:49 +0300

    validator added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import 'bootstrap';
 import './app.scss';
-import $ from 'jquery';
+// import $ from 'jquery';
 import validator from 'validator';
 
 const state = {
@@ -10,3 +10,28 @@ const state = {
   },
   feeds: [],
 };
+
+const form = document.getElementById('feedUrlForm');
+const input = document.getElementById('feedUrlInput');
+
+const onInputChange = () => {
+  const value = input.value.trim();
+  state.input.value = value;
+  state.input.isValid = !input.value || validator.isURL(value);
+  if (state.input.isValid) {
+    input.classList.remove('invalidInput');
+  } else {
+    input.classList.add('invalidInput');
+  }
+};
+
+input.addEventListener('change', () => {
+  onInputChange();
+});
+input.addEventListener('input', () => {
+  onInputChange();
+});
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+});
