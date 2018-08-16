1b792a22c50370520f0be04c7d39e55462c01594
Alexey Panteleychuk <alexeypanteleychuk@MacBook-Pro-Alexey.local>
2018-08-16 16:24:12 +0300

    PubSub WORK!

--- src/index.js
+++ src/index.js
@@ -5,8 +5,7 @@ import validator from 'validator';
 import PubSub from './PubSub';
 
 const state = {
-  feeds: ['ti.com'],
-  news: [],
+  feeds: [],
 };
 
 const events = new PubSub();
@@ -34,91 +33,23 @@ form.addEventListener('submit', (e) => {
   }
 });
 
-
-// const onFormSubmit = () => {
-//   if (state.input.isValid) {
-//     const previousFeeds = state.feeds;
-//     state.feeds = [state.input.value, ...previousFeeds];
-//   }
-//   state.input.value = '';
-//   state.input.isValid = true;
-// };
-
-const newsToAdd = (newFeed) => {
-  state.news = [newFeed.value, ...state.news];
-  const $el = $('#newsList');
-  $el.html(state.news);
+const feedToAdd = (feedUrl) => {
+  if (!state.feeds.includes(feedUrl)) {
+    state.feeds = [feedUrl, ...state.feeds];
+    events.emit('feedAdded');
+  }
 };
 
-events.on('feedSubmitted', newsToAdd);
+const generateDescriptionList = () => {
+  const $el = $('#feedsDescriptionList');
+  $el.html(state.feeds.join(';'));
+};
 
-// const input = {
-//   input: {
-//     value: '',
-//     isValid: true,
-//   },
-//   init() {
-//     this.cacheDom();
-//     this.bindEvents();
-//   },
-//   cacheDom() {
-//     this.$el = $('#feedUrlForm');
-//     this.$input = this.$el.find('input');
-//     this.$button = this.$el.find('button');
-//   },
-//   bindEvents() {
-//     this.$input.on('input', this.getInput.bind(this));
-//     this.$button.on('click', this.submitFeed.bind(this));
-//   },
-//   getInput() {
-//     const value = this.$input.val().trim();
-//     this.input.value = value;
-//     this.input.isValid = !this.input.value || validator.isURL(value);
-//     if (this.input.isValid) {
-//       this.$input.removeClass('is-invalid');
-//     } else {
-//       this.$input.addClass('is-invalid');
-//     }
-//   },
-//   submitFeed() {
-//     this.$input.val('');
-//     events.emit('feedSubmitted', this.input);
-//   },
-// };
+const generateNewsList = () => {
+  const $el = $('#newsList');
+  $el.html(state.feeds.join(','));
+};
 
-// const feeds = {
-//   feeds: ['http://analog.com', 'http://ti.com'],
-//   init() {
-//     this.cacheDom();
-//     // this.render();
-//     events.on('feedSubmitted', this.addFeed.bind(this));
-//   },
-//   cacheDom() {
-//     this.$el = $('#feedsList');
-//     this.$p = this.$el.find('p');
-//   },
-//   render() {
-//     const data = this.feeds.join('');
-//     this.$p.html(data);
-//     console.log(data);
-//   },
-//   addFeed(newFeed) {
-//     this.feeds = [newFeed.value, ...this.feeds];
-//     this.render();
-//   },
-// };
-
-// const news = {
-//   newsList: [],
-//   init() {
-//     events.on('feedSubmitted', this.addNews.bind(this));
-//   },
-//   addNews(newFeed) {
-//     this.newsList = [newFeed.value, ...this.feeds];
-//     // this.render();
-//   },
-// };
-
-// input.init();
-// feeds.init();
-// news.init();
+events.on('feedSubmitted', feedToAdd);
+events.on('feedAdded', generateDescriptionList);
+events.on('feedAdded', generateNewsList);
