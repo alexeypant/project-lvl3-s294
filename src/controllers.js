3073b422f7eadf7add0f0287a215fc1d73bdb1c2
Alexey Panteleychuk <alexeypanteleychuk@MacBook-Pro-Alexey.local>
2018-08-18 17:29:33 +0300

    implement setters for state

--- src/controllers.js
+++ src/controllers.js
@@ -2,29 +2,27 @@ import axios from 'axios';
 import _ from 'lodash';
 import { getTitleAndDescriptionFromXml, getArticlesFromXml } from './xmlReader';
 
-export const onFeedAdded = (state, updateState) => {
-  const { urls } = state;
-  if (urls.length === 0) return;
+export const onFeedAdded = (state) => {
   const proxyURL = 'https://cors-anywhere.herokuapp.com/';
-  const downloadPromises = urls.map(url => axios.get(`${proxyURL}${url}`));
+  const downloadPromises = state.urls.map(url => axios.get(`${proxyURL}${url}`));
   Promise.all(downloadPromises)
     .then((xmls) => {
-      updateState({ ...state, xmls });
+      state.setXmls(xmls);
     })
-    .catch((error) => {
-      console.log(error);
+    .catch(() => {
+      setTimeout(onFeedAdded(state), 5000);
     })
     .then(() => {
       setTimeout(onFeedAdded(state), 5000);
     });
 };
 
-export const onXmlsReceived = (state, updateState) => {
+export const onXmlsReceived = (state) => {
   const parser = new DOMParser();
   const docs = state.xmls.map(el => parser.parseFromString(el.data, 'application/xml'));
   const titles = docs.map(doc => getTitleAndDescriptionFromXml(doc));
   if (state.titles.length !== titles.length) {
-    updateState({ ...state, titles });
+    state.setTitles(titles);
   }
   const articlesNotFlat = docs.map(doc => getArticlesFromXml(doc));
   const articles = _.flatten(articlesNotFlat);
@@ -32,6 +30,6 @@ export const onXmlsReceived = (state, updateState) => {
   const newArticles = articles.filter(art => !excistingArticlesTitles.includes(art.title));
   if (newArticles.length > 0) {
     const updatedArticlesList = [...newArticles, ...state.articles];
-    updateState({ ...state, articles: updatedArticlesList });
+    state.setArticles(updatedArticlesList);
   }
 };
