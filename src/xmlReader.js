228c588f6b7b4fe96bb8465a86b60651e7d8a582
Alexey Panteleychuk <alexeypanteleychuk@MacBook-Pro-Alexey.local>
2018-08-19 17:09:28 +0300

    comments removed

--- src/xmlReader.js
+++ src/xmlReader.js
@@ -24,27 +24,3 @@ export default (xml) => {
     articles,
   };
 };
-
-// export const getTitleAndDescriptionFromXml = (xmlDOM) => {
-//   const title = xmlDOM.querySelector('channel > title');
-//   const description = xmlDOM.querySelector('channel > description');
-//   return {
-//     title: title ? title.textContent : '',
-//     description: description ? description.textContent : '',
-//   };
-// };
-
-// export const getArticlesFromXml = (xmlDOM) => {
-//   const items = xmlDOM.querySelectorAll('item');
-//   const articles = [].map.call(items, (item) => {
-//     const articleTitle = item.querySelector('title');
-//     const articleLink = item.querySelector('link');
-//     const articleDescription = item.querySelector('description');
-//     return {
-//       title: articleTitle ? articleTitle.textContent : '',
-//       link: articleLink ? articleLink.textContent.trim() : '',
-//       description: articleDescription ? articleDescription.textContent : '',
-//     };
-//   });
-//   return articles;
-// };
