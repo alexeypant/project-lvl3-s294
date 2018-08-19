5ce233e3aac76f77be725d2b0e568702c66255c1
Alexey Panteleychuk <alexeypanteleychuk@MacBook-Pro-Alexey.local>
2018-08-19 17:00:01 +0300

    every new added feed is downloaded separately

--- src/controllers.js
+++ src/controllers.js
@@ -1,39 +1,54 @@
 import axios from 'axios';
 import _ from 'lodash';
 import validator from 'validator';
-import { getTitleAndDescriptionFromXml, getArticlesFromXml } from './xmlReader';
+import { getTitleAndDescriptionFromXml, getArticlesFromXml, parseXml } from './xmlReader';
 
 export const onInputChanged = (state) => {
   const isValid = !state.input || validator.isURL(state.input);
   state.updateIsInputValid(isValid);
 };
 
-const parser = new DOMParser();
+// const parser = new DOMParser();
+
+// const onFeedAddedOld = (state) => {
+//   const proxyURL = 'https://cors-anywhere.herokuapp.com/';
+//   const downloadPromises = state.urls.map(url => axios.get(`${proxyURL}${url}`));
+//   Promise.all(downloadPromises)
+//     .then((xmls) => {
+//       const docs = xmls.map(el => parser.parseFromString(el.data, 'application/xml'));
+//       const titles = docs.map(doc => getTitleAndDescriptionFromXml(doc));
+//       if (state.titles.length !== titles.length) {
+//         state.updateTitles(titles);
+//       }
+//       const articlesNotFlat = docs.map(doc => getArticlesFromXml(doc));
+//       const articles = _.flatten(articlesNotFlat);
+//       const excistingArticlesTitles = state.articles.map(art => art.title);
+//       const newArticles = articles.filter(art => !excistingArticlesTitles.includes(art.title));
+//       if (newArticles.length > 0) {
+//         const updatedArticlesList = [...newArticles, ...state.articles];
+//         state.updateArticles(updatedArticlesList);
+//       }
+//     })
+//     .catch(() => {
+//       setTimeout(onFeedAdded(state), 5000);
+//     })
+//     .then(() => {
+//       setTimeout(onFeedAdded(state), 5000);
+//     });
+// };
+
 
 const onFeedAdded = (state) => {
   const proxyURL = 'https://cors-anywhere.herokuapp.com/';
-  const downloadPromises = state.urls.map(url => axios.get(`${proxyURL}${url}`));
-  Promise.all(downloadPromises)
-    .then((xmls) => {
-      const docs = xmls.map(el => parser.parseFromString(el.data, 'application/xml'));
-      const titles = docs.map(doc => getTitleAndDescriptionFromXml(doc));
-      if (state.titles.length !== titles.length) {
-        state.updateTitles(titles);
-      }
-      const articlesNotFlat = docs.map(doc => getArticlesFromXml(doc));
-      const articles = _.flatten(articlesNotFlat);
-      const excistingArticlesTitles = state.articles.map(art => art.title);
-      const newArticles = articles.filter(art => !excistingArticlesTitles.includes(art.title));
-      if (newArticles.length > 0) {
-        const updatedArticlesList = [...newArticles, ...state.articles];
-        state.updateArticles(updatedArticlesList);
-      }
+  const download = axios.get(`${proxyURL}${state.urls[0]}`);
+  (download)
+    .then((xml) => {
+      const { titles, articles } = parseXml(xml);
+      state.updateTitles([titles, ...state.titles]);
+      state.updateArticles([...articles, ...state.articles]);
     })
-    .catch(() => {
-      setTimeout(onFeedAdded(state), 5000);
-    })
-    .then(() => {
-      setTimeout(onFeedAdded(state), 5000);
+    .catch((error) => {
+      console.log(error);
     });
 };
 
--- src/xmlReader.js
+++ src/xmlReader.js
@@ -21,3 +21,29 @@ export const getArticlesFromXml = (xmlDOM) => {
   });
   return articles;
 };
+
+export const parseXml = (xml) => {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(xml.data, 'application/xml');
+  const title = doc.querySelector('channel > title');
+  const description = doc.querySelector('channel > description');
+  const items = doc.querySelectorAll('item');
+  const articles = [].map.call(items, (item) => {
+    const articleTitle = item.querySelector('title');
+    const articleLink = item.querySelector('link');
+    const articleDescription = item.querySelector('description');
+    return {
+      title: articleTitle ? articleTitle.textContent : '',
+      link: articleLink ? articleLink.textContent.trim() : '',
+      description: articleDescription ? articleDescription.textContent : '',
+    };
+  });
+
+  return {
+    titles: {
+      title: title ? title.textContent : '',
+      description: description ? description.textContent : '',
+    },
+    articles,
+  };
+};
